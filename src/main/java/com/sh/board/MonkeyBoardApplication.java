8d865fc022b547d0a710436bf803c8392123ca5e
Gonue <6017891@naver.com>
2023-02-27 02:35:06 +0900

    [add] firstDependencies&ignore.io

---
+++ src/main/java/com/sh/board/MonkeyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sh.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/board/MonkeyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
