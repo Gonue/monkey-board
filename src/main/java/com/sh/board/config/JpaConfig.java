00ba38fbf014dd70e8907a37bc099c2cb5adc770
Gonue <6017891@naver.com>
2023-02-27 22:37:47 +0900

    [add] jpaSetYaml
    [add] auditing
    [add] bootProperties
    [TODO]external 환경주입필요

---
+++ src/main/java/com/sh/board/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.sh.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("kim"); // TODO: Spring Security 인증 기능을 붙이게 될때 수정
+    }
+}
