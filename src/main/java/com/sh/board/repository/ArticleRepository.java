e3c51ee1d22808745d6ab2dd052ba0be05c1ad42
Gonue <6017891@naver.com>
2023-02-28 02:56:23 +0900

    [add] dataRest

--- src/main/java/com/sh/board/repository/ArticleRepository.java
+++ src/main/java/com/sh/board/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.sh.board.repository;
 
 import com.sh.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
