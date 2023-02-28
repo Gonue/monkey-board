0f351fddc6b5f53d705ffc6cbdd1ddcfffcd258d
Gonue <6017891@naver.com>
2023-03-01 00:59:02 +0900

    [add]articleSkeleton
    controller,view,test

--- src/test/java/com/sh/board/controller/ArticleControllerTest.java
+++ src/test/java/com/sh/board/controller/ArticleControllerTest.java
@@ -39,8 +39,8 @@ class ArticleControllerTest {
             //When & Then
             mvc.perform(get("/articles/1"))
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.TEXT_HTML))
-                    .andExpect(view().name("articles/index"))
+                    .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                    .andExpect(view().name("articles/detail"))
                     .andExpect(model().attributeExists("article"))
                     .andExpect(model().attributeExists("articleComments"));
         }
@@ -52,7 +52,7 @@ class ArticleControllerTest {
                 //When & Then
                 mvc.perform(get("/articles/search"))
                         .andExpect(status().isOk())
-                        .andExpect(content().contentType(MediaType.TEXT_HTML))
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                         .andExpect(model().attributeExists("article/search"));
 
             }
@@ -64,7 +64,7 @@ class ArticleControllerTest {
                 //When & Then
                 mvc.perform(get("/articles/search-hashtag"))
                         .andExpect(status().isOk())
-                        .andExpect(content().contentType(MediaType.TEXT_HTML))
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                         .andExpect(model().attributeExists("article/search-hashtag"));
             }
 }
