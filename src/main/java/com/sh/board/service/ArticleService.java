393f274d3baad2983577f9ad276667ccc807ac58
Gonue <6017891@naver.com>
2023-03-03 04:02:09 +0900

    [add] articleBusinessLogic & articleController
    게시판 서비스 테스트 및 비즈니스 로직
    + 간단한 view 테스트

--- src/main/java/com/sh/board/service/ArticleService.java
+++ src/main/java/com/sh/board/service/ArticleService.java
@@ -1,37 +1,88 @@
 package com.sh.board.service;
 
+import com.sh.board.domain.Article;
 import com.sh.board.domain.type.SearchType;
 import com.sh.board.dto.ArticleDto;
 import com.sh.board.dto.ArticleWithCommentsDto;
 import com.sh.board.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
+import static com.sh.board.domain.type.SearchType.CONTENT;
+import static com.sh.board.domain.type.SearchType.TITLE;
+
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+
+    //TODO: 나중에 다시 리팩토링 예정,,****
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        if(searchKeyword == null || searchKeyword.isBlank()){
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        Page<ArticleDto> result = Page.empty();
+                switch (searchType){
+                case TITLE:
+                    result = articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+                    break;
+                case CONTENT:
+                    result = articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+                    break;
+                case ID:
+                    result = articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+                    break;
+                case NICKNAME:
+                    result = articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+                    break;
+                case HASHTAG:
+                    result = articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+                    break;
+                default:
+                    break;
+            }
+            return result;
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.getId());
+            if(dto.getTitle() != null){
+                article.setTitle(dto.getTitle());
+            }
+            if(dto.getContent() != null){
+                article.setContent(dto.getContent());
+            }
+            article.setHashtag(dto.getHashtag());
+            articleRepository.save(article);
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패, 게시글 찾지 못함 dto :" +  dto);
+        }
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
--- src/test/java/com/sh/board/controller/ArticleControllerTest.java
+++ src/test/java/com/sh/board/controller/ArticleControllerTest.java
@@ -1,15 +1,33 @@
 package com.sh.board.controller;
 
 import com.sh.board.config.SecurityConfig;
+import com.sh.board.dto.ArticleWithCommentsDto;
+import com.sh.board.dto.UserAccountDto;
+import com.sh.board.service.ArticleService;
+import org.assertj.core.api.BDDAssertions;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
+    @MockBean private ArticleService articleService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -26,27 +45,32 @@ class ArticleControllerTest {
     @Test
     public void given_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         //Given
-
+        BDDMockito.given(articleService.searchArticles(ArgumentMatchers.eq(null),ArgumentMatchers.eq(null), any(Pageable.class)))
+                        .willReturn(Page.empty());
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
+        BDDMockito.then(articleService).should().searchArticles(eq(null),eq(null), any(Pageable.class));
     }
     @DisplayName("[view] {GET} 게시글 상세 페이지 - 정상호출")
         @Test
         public void given_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
             //Given
-
+            Long articleId = 1L;
+            BDDMockito.given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
             //When & Then
-            mvc.perform(get("/articles/1"))
+            mvc.perform(get("/articles/" + articleId))
                     .andExpect(status().isOk())
                     .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                     .andExpect(view().name("articles/detail"))
                     .andExpect(model().attributeExists("article"))
                     .andExpect(model().attributeExists("articleComments"));
+            BDDMockito.then(articleService).should().getArticle(articleId);
         }
+    @Disabled("구현 중")
     @DisplayName("[view] {GET} 게시글 검색 전용 페이지 - 정상호출")
             @Test
             public void given_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
@@ -59,6 +83,7 @@ class ArticleControllerTest {
                         .andExpect(model().attributeExists("article/search"));
 
             }
+    @Disabled("구현 중")
     @DisplayName("[view] {GET} 게시글 해시태그 검색 페이지 - 정상호출")
             @Test
             public void given_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
@@ -70,4 +95,34 @@ class ArticleControllerTest {
                         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                         .andExpect(model().attributeExists("article/search-hashtag"));
             }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto(){
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "kim",
+                LocalDateTime.now(),
+                "kim"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto(){
+        return UserAccountDto.of(
+                1L,
+                "Kim",
+                "pw",
+                "kim@gmail.com",
+                "Kim",
+                "memo",
+                LocalDateTime.now(),
+                "kim",
+                LocalDateTime.now(),
+                "kim"
+                );
+    }
 }
