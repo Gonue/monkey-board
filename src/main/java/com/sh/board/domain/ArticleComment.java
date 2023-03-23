f3188eb5ddbbd2a7a7f047fc7ab5a6be997ab02b
Gonue <6017891@naver.com>
2023-03-24 02:18:55 +0900

    [fix] articleCommentDesign
    대댓글을 위해 엔티티 수정

--- src/main/java/com/sh/board/domain/ArticleComment.java
+++ src/main/java/com/sh/board/domain/ArticleComment.java
@@ -11,7 +11,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -34,6 +36,16 @@ public class ArticleComment extends AuditingFields{
     @JoinColumn(name = "userId")
     private UserAccount userAccount;
 
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId;
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+
     @Setter @Column(nullable = false, length = 500)
     private String content;
 
@@ -41,13 +53,19 @@ public class ArticleComment extends AuditingFields{
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount =userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
-    public static ArticleComment of(Article article,UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+    public static ArticleComment of(Article article,UserAccount userAccount, Long parentCommentId, String content) {
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child){
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
