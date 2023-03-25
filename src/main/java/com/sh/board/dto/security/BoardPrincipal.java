87e7cde945b0ef42de759126e6391cbc0b57ee83
Gonue <6017891@naver.com>
2023-03-26 04:40:44 +0900

    [fix]boardPrincipal
    OAuth2 정보를 받을수 있게 수정

--- src/main/java/com/sh/board/dto/security/BoardPrincipal.java
+++ src/main/java/com/sh/board/dto/security/BoardPrincipal.java
@@ -6,22 +6,28 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Data
-public class BoardPrincipal implements UserDetails {
+public class BoardPrincipal implements UserDetails, OAuth2User {
     private final String username;
     private final String password;
     private final Collection<? extends GrantedAuthority> authorities;
     private final String email;
     private final String nickname;
     private final String memo;
-
+    private final Map<String, Object> oAuth2Attributes;
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+            return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
         return new BoardPrincipal(
                 username,
@@ -32,7 +38,8 @@ public class BoardPrincipal implements UserDetails {
                         .collect(Collectors.toUnmodifiableSet()),
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -102,5 +109,16 @@ public class BoardPrincipal implements UserDetails {
             this.name = name;
         }
 
+    }
+
+    //OAuth2
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attributes;
+    }
+
+    @Override
+    public String getName() {
+        return username;
     }
 }
